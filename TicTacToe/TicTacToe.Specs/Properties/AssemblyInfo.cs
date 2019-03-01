50326841c34fba3ab443e1ca5b319830b9ccd865
Luc Leysen werk <luc.leysen@cegeka.com>
2019-03-01 13:32:45 +0100

    Setup project

--- TicTacToe/TicTacToe.Specs/Properties/AssemblyInfo.cs
+++ TicTacToe/TicTacToe.Specs/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("TicTacToe")]
+[assembly: AssemblyTitle("TicTacToe.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TicTacToe")]
+[assembly: AssemblyProduct("TicTacToe.Specs")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("49b294d4-2a57-4add-830a-4f72b9529d28")]
+[assembly: Guid("b5615a91-b4b9-4daa-90ea-52d69280fa82")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
